658b9ef57dd79f22b2da43ab5eafd34036b8c3f5
Paul <paulnyamawi18@gmail.com>
2025-02-14 22:50:13 +0300

    creating  simple system

---
+++ expense-tracker-ui/src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-14 m-auto">
+    <h1 class="text-gray-200 font-semibold font-sans text-xl">{{ text }}</h1>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: String,
+});
+</script>
