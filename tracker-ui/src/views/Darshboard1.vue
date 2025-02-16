8bb6af0a7f17680f228edd6684540863c0b99e56
Paul <paulnyamawi18@gmail.com>
2025-02-16 15:38:32 +0300

    dealing with frontend part

--- tracker-ui/src/views/Darshboard1.vue
+++ tracker-ui/src/views/Darshboard1.vue
@@ -9,20 +9,7 @@
           class="flex items-center justify-center flex-shrink-0 w-full h-16"
           href="#"
         >
-          <svg
-            class="w-8 h-8 stroke-current text-gray-300"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
-            />
-          </svg>
+        <font-awesome-icon :icon="['fas', 'business-time']" class="text-xl text-gray-300"/>
         </a>
         <a
           class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-800"
