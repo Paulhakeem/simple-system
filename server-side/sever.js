8f51349c225f712151dea3bf1287b492fb4aa64b
Paul <paulnyamawi18@gmail.com>
2025-02-17 23:13:54 +0300

    setting up backend with express and mongoDB

--- server-side/sever.js
+++ server-side/sever.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const env = require('dotenv')
-const bodyParser = require('body-parser')
+const express = require("express");
+const env = require("dotenv");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require('cors')
+const logger = require('morgan')
 
-const app = express()
+env.config({ path: "/.config.env" });
 
+const app = express();
 
-app.listen(8000, async()=> {
-console.log('Server is running in 8000..');
+app.use(bodyParser.json());
+app.use(logger('dev'))
+app.use(cors({origin: "*"}))
+app.use(express.urlencoded({extended: false}))
 
-})
+mongoose
+  .connect('mongodb+srv://admin:gPlXjgJBXSGc2COQ@cluster0.2m2gpna.mongodb.net/cinemax?retryWrites=true&w=majority&appName=Cluster0', {})
+  .then((result) => {
+    console.log("Database connection successfully");
+  })
+  .catch((err) => {
+    console.log("Database connection field");
+  });
+
+app.listen(8000, async () => {
+  console.log("Server is running on port 8000..");
+});
