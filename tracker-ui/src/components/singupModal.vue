7d719da4b9271bb4fde9fa7fb98009f4cd20fdc7
Paul <paulnyamawi18@gmail.com>
2025-02-19 22:19:58 +0300

    done with signup modal

---
+++ tracker-ui/src/components/singupModal.vue
@@ -0,0 +1,119 @@
+<template>
+  <transition name="modal-outer">
+    <div
+      v-show="modalActive"
+      class="absolute z-50 bg-black bg-opacity-0 w-full h-screen top-0 left-0 bg-opacity-30 flex justify-center"
+    >
+      <transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="max-w-md self-start p-4 bg-gray-400 mt-36 rounded-md"
+        >
+          <button>
+            <font-awesome-icon
+              @click="$emit('close-modal')"
+              :icon="['fas', 'circle-xmark']"
+              class="text-lg text-[#1796d5] cursor-pointer text-right flex justify-end p-2"
+            />
+          </button>
+
+          <!-- SLOT -->
+          <slot>
+            <div class="bg-gray-500 p-4 rounded-md">
+              <!-- component -->
+              <div
+                class="my-2 mx-auto border-[2px] w-10/12 justify-center flex items-center rounded-md shadow-md"
+              >
+                <div>
+                  <button
+                    class="flex items-center bg-gray-400 rounded-l-md border border-white justify-center w-12 h-12 text-white"
+                  >
+                    <font-awesome-icon
+                      :icon="['fas', 'user']"
+                      class="text-white text-lg"
+                    />
+                  </button>
+                </div>
+
+                <div class="w-full">
+                  <input
+                    type="text"
+                    class="w-full h-12 px-4 py-1 rounded-r-md border border-gray-100 text-gray-800 focus:outline-none"
+                    placeholder="Name"
+                  />
+                </div>
+              </div>
+              <div
+                class="my-2 mx-auto border-[2px] w-10/12 justify-center flex items-center rounded-md shadow-md"
+              >
+                <div>
+                  <button
+                    class="flex items-center bg-gray-400 rounded-l-md border border-white justify-center w-12 h-12 text-white"
+                  >
+                    <font-awesome-icon
+                      :icon="['fas', 'key']"
+                      class="text-white text-lg"
+                    />
+                  </button>
+                </div>
+
+                <div class="w-full">
+                  <input
+                    type="password"
+                    class="w-full h-12 px-4 py-1 rounded-r-md border border-gray-100 text-gray-800 focus:outline-none"
+                    placeholder="Passoward"
+                  />
+                </div>
+              </div>
+              <div class="my-2 mx-auto w-10/12 justify-end flex items-end">
+                <button
+                  class="bg-gray-600 rounded-lg px-3 py-2 text-white cursor-pointer"
+                >
+                  Signup
+                </button>
+              </div>
+            </div>
+          </slot>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+defineEmits(["close-modal"]);
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+.modal-inner-enter-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from{
+    opacity: 0;
+    transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+    transform: scale(0.8);
+}
+</style>
--- tracker-ui/src/views/HomeView.vue
+++ tracker-ui/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup>
-import { RouterLink } from 'vue-router';
-import HomePage from '@/components/HomePage.vue';
-import formInputModal from "@/components/formInputModal.vue";
-import { ref } from 'vue';
+import { RouterLink } from "vue-router";
+import HomePage from "@/components/HomePage.vue";
+import SignupModal from "@/components/singupModal.vue";
+
+import { ref } from "vue";
 
 const header = ref("CONTROL YOUR EXPENSES");
 
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
 
 <template>
@@ -14,49 +19,68 @@ const header = ref("CONTROL YOUR EXPENSES");
     <div class="flex space-x-7 justify-center pt-6">
       <RouterLink to="">
         <button
+         @click="toggleModal"
           class="border-2 border-[#1796d5] rounded-lg px-3 py-2 text-[#1796d5] cursor-pointer hover:bg-[#1796d5] hover:text-gray-200"
         >
-          Get Started
+        Signup
         </button>
       </RouterLink>
       <RouterLink to="">
         <button
-          class="border-2 border-[#1796d5] rounded-lg px-3 py-2 w-26 text-[#1796d5] cursor-pointer hover:bg-[#1796d5] hover:text-gray-200"
+          class="border-2 border-[#1796d5] rounded-lg px-3 py-2 text-[#1796d5] cursor-pointer hover:bg-[#1796d5] hover:text-gray-200"
         >
-          Explore
+         Signin
         </button>
       </RouterLink>
     </div>
 
     <!-- BUTTONS -->
-    <div
-    class="bg-[#222327] my-14 p-8 rounded-tl-4xl rounded-tr-4xl"
-  >
-    <div class="max-w-lg m-auto justify-center text-left">
-     <div class="flex items-center justify-between">
-        <h1 class="text-lg font-semibold text-[#1796d5]">Catergorise</h1>
-        <font-awesome-icon :icon="['fas', 'square-plus']" class="text-2xl text-[#1796d5] cursor-pointer"/>
-     </div>
-
-      <!-- catergories -->
-      <div class="pt-2">
-        <div class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer">
-            <font-awesome-icon :icon="['fas', 'car']" class="text-xl text-[#1796d5]"/>
-          <p class="text-gray-300">Travel & Tours</p>
+    <div class="bg-[#222327] my-14 p-8 rounded-tl-4xl rounded-tr-4xl">
+      <div class="max-w-lg m-auto justify-center text-left">
+        <div class="flex items-center justify-between">
+          <h1 class="text-lg font-semibold text-[#1796d5]">Catergorise</h1>
+          <font-awesome-icon
+            :icon="['fas', 'square-plus']"
+            class="text-2xl text-[#1796d5] cursor-pointer"
+          />
         </div>
-        <RouterLink to="/darshboard1" class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer">
-            <font-awesome-icon :icon="['fab', 'shopify']" class="text-xl text-[#1796d5]"/>
+
+        <!-- catergories -->
+        <div class="pt-2">
+          <div
+            class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer"
+          >
+            <font-awesome-icon
+              :icon="['fas', 'car']"
+              class="text-xl text-[#1796d5]"
+            />
+            <p class="text-gray-300">Travel & Tours</p>
+          </div>
+          <RouterLink
+            to="/darshboard1"
+            class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer"
+          >
+            <font-awesome-icon
+              :icon="['fab', 'shopify']"
+              class="text-xl text-[#1796d5]"
+            />
             <p class="text-gray-300">Shopping</p>
-        </RouterLink >
-      
-        <div class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer">
-            <font-awesome-icon :icon="['fas', 'cow']" class="text-xl text-[#1796d5]"/>
+          </RouterLink>
+
+          <div
+            class="bg-gray-600 my-4 max-w-lg m-auto p-4 flex gap-4 items-center cursor-pointer"
+          >
+            <font-awesome-icon
+              :icon="['fas', 'cow']"
+              class="text-xl text-[#1796d5]"
+            />
             <p class="text-gray-300">Agriculture</p>
+          </div>
         </div>
       </div>
     </div>
-    <formInputModal/>
-  </div>
+
+    <SignupModal :modalActive="modalActive" @close-modal="toggleModal">
+    </SignupModal>
   </main>
- 
 </template>
