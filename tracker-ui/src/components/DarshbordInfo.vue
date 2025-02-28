35ae05a06447d9e1a2a9301ec0288e742fe5cb7b
Paul <paulnyamawi18@gmail.com>
2025-02-28 22:19:12 +0300

    responsiveness

--- tracker-ui/src/components/DarshbordInfo.vue
+++ tracker-ui/src/components/DarshbordInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-grow p-6 overflow-auto bg-gray-800">
-    <div class="grid grid-cols-3 gap-6">
+    <div id="box" class="grid grid-cols-3 gap-6">
       <!-- TOTAL MONEY -->
       <div class="h-auto col-span-1 bg-gray-700">
         <div class="text-center my-4">
@@ -45,30 +45,12 @@
           <h6 class="text-sm">Money used to pay bills</h6>
         </div>
       </div>
+    </div>
 
-      <!-- BUSINESS STATEMENTS -->
-      <div class="h-auto col-span-2 bg-gray-700 py-1">
+       <!-- BUSINESS STATEMENTS -->
+    <div class="h-auto col-span-1 bg-gray-700 py-1 w-full mt-10">
         <Statement />
       </div>
-
-      <!-- insights -->
-      <div class="h-auto col-span-1 bg-gray-700">
-        <div class="text-center pt-2">
-          <h1 class="text-[#1796d5] text-lg font-regular">Insights</h1>
-        </div>
-        <div class="flex items-center my-4 mx-2">
-          <span class="mr-2">60%</span>
-          <div class="relative w-full">
-            <div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-              <div
-                style="width: 60%"
-                class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-400"
-              ></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -78,3 +60,7 @@ import { useTransStore } from "../../store/transaction";
 
 const trans = useTransStore()
 </script>
+
+<style>
+
+</style>
