19bc8c67f33a76fa6668100a2c5093042bfa501e
Paul <paulnyamawi18@gmail.com>
2025-03-03 22:34:52 +0300

    time to sleep

--- server-side/routers/transRouter.js
+++ server-side/routers/transRouter.js
@@ -6,7 +6,6 @@ const router = express.Router()
 
 router.route('/transactions').post(trans.createtransaction)
 router.route('/statements').get(trans.getTrans)
-router.route('/filter').get(trans.filterData)
 
 
 module.exports = router
