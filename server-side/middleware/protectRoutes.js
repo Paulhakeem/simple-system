6bc5fc28dc35505b625a22061a899d878f9a0b66
Paul <paulnyamawi18@gmail.com>
2025-03-11 21:25:09 +0300

    protecting routes

--- server-side/middleware/protectRoutes.js
+++ server-side/middleware/protectRoutes.js
@@ -1,9 +1,31 @@
-exports.isAuthenticated = (req, res, next) => {
-  if (req.session.userId) {
-    return next();
+const util = require("util");
+const jwt = require("jsonwebtoken");
+const env = require("dotenv");
+const Users = require("../model/users");
+
+env.config({ path: "./.env" });
+
+exports.isAuthenticated = async (req, res, next) => {
+  const userToken = req.headers.authorization;
+  let tokens;
+  if (userToken && userToken.startsWith("Bearer")) {
+    tokens = userToken.split("")[1];
   }
-  res.status(401).json({
-    statusCode: 401,
-    message: 'Unauthorized'
-  })
+  const promisifyToken = await util.promisify(jwt.verify)(
+    tokens,
+    process.env.SECRET_TOKEN
+  );
+
+  const user = await Users.findById(promisifyToken.id);
+  if (!user) {
+    res.status(404).json({
+      statusCode: 404,
+      message: "User Not Found!",
+    });
+  }
+  res.status(200).json({
+    statusCode: 200,
+    message: "User Found",
+  });
+  next()
 };
