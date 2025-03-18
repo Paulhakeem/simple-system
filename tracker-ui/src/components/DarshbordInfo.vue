ca4a7bf0665833e080e3a2d5819a8a3e8999ed60
Paul <paulnyamawi18@gmail.com>
2025-03-18 23:31:13 +0300

    responsiveness

--- tracker-ui/src/components/DarshbordInfo.vue
+++ tracker-ui/src/components/DarshbordInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex-grow p-6 overflow-auto bg-gray-800">
-    <div id="box" class="grid grid-cols-3 gap-6">
+    <div class="grid grid-cols-3 gap-6">
       <!-- TOTAL MONEY -->
-      <div class="h-auto col-span-1 bg-gray-700">
-        <div class="text-center my-4">
+      <div class="h-auto bg-gray-700 W-96">
+        <div id="flex-box" class="text-center my-4">
           <h5>Income(Ksh)</h5>
           <div class="py-2">
             <h1 class="text-4xl font-bold text-green-400">{{ trans.income }}</h1>
           </div>
-          <div class="flex gap-2 justify-center items-center">
+          <div id="flex-box-span" class="flex gap-2 justify-center items-center">
             <span class="text-sm">Total Amount:</span>
             <h6 class="text-lg font-semibold"><span class="text-xs">Ksh</span>{{ trans.totalAmout }}</h6>
           </div>
@@ -16,9 +16,9 @@
       </div>
 
       <!-- ITRACKING -->
-      <div class="h-auto col-span-1 bg-gray-700 flex items-center justify-center">
-        <div class="text-center my-4 flex space-x-10">
-         <div>
+      <div class="h-auto bg-gray-700 flex items-center justify-center">
+        <div id="progress" class="text-center my-4 flex space-x-10">
+         <div id="col">
             <p class="py-1">Money In</p>
             <div class="flex gap-2">
             <span class="text-green-400 text-md">{{ trans.getIncome }}%</span>
@@ -37,7 +37,7 @@
 
       <!-- EXPENSES -->
       <div class="h-auto col-span-1 bg-gray-700">
-        <div class="text-center my-4">
+        <div id="flex-box" class="text-center my-4">
           <h5>Epenses(Ksh)</h5>
           <div class="py-2">
             <h1 class="text-4xl font-bold text-red-500">{{ trans.totalExpenses }}</h1>
@@ -60,7 +60,3 @@ import { useTransStore } from "../../store/transaction";
 
 const trans = useTransStore()
 </script>
-
-<style>
-
-</style>
