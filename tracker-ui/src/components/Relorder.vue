ff8e5156d489b386a356a3d520fa3612ff31eb88
Paul <paulnyamawi18@gmail.com>
2025-03-23 19:44:45 +0300

    adding skeleton loader

--- tracker-ui/src/components/Relorder.vue
+++ tracker-ui/src/components/Relorder.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 mt-4 w-full"
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 mt-4 w-full justify-center items-center"
   >
     <div class="animate-pulse flex space-x-4">
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-200 rounded w-24"></div>
+        <div class="h-4 bg-gray-400 rounded w-24"></div>
       </div>
     </div>
 
     <div class="animate-pulse flex space-x-4">
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-200 rounded w-24"></div>
+        <div class="h-4 bg-gray-400 rounded w-24"></div>
       </div>
     </div>
   </div>
