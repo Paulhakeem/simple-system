efc5075f2a6abffd76ecb4240c6f8f206e6c664b
Paul <paulnyamawi18@gmail.com>
2025-04-11 13:51:50 +0300

    adding filter component

--- tracker-ui/src/components/Statement.vue
+++ tracker-ui/src/components/Statement.vue
@@ -2,12 +2,16 @@
   <div class="w-full px-4 mx-auto">
     <div class="rounded-t mb-0 px-4 py-3 border-0">
       <div class="flex flex-wrap items-center">
+        
         <!-- filter -->
         <font-awesome-icon
+          @click="toggleModal"
           :icon="['fas', 'ellipsis-vertical']"
           class="text-[#1796d5] cursor-pointer"
         />
+        <FilterTrans v-if="openModal" />
         <!--  filter-->
+
         <div class="relative w-full px-4 max-w-full flex-grow flex-1">
           <h3 class="font-semibold text-base text-blueGray-700">
             Business Name
@@ -32,9 +36,9 @@
         </div>
       </div>
     </div>
-    
+
     <div class="block w-full overflow-x-auto">
-      <TableTrans/>
+      <TableTrans />
     </div>
   </div>
 </template>
@@ -44,9 +48,11 @@ import { ref, onMounted } from "vue";
 import { useUserStore } from "../../store/user";
 import addTrans from "./addTrans.vue";
 import TableTrans from "./TableTrans.vue";
+import FilterTrans from "./FilterTrans.vue";
 
 const showInput = ref(false);
 const { getUser } = useUserStore();
+const openModal = ref(false);
 
 const toggleInput = () => {
   showInput.value = !showInput.value;
@@ -54,6 +60,10 @@ const toggleInput = () => {
 
 const hideInput = () => {
   showInput.value = false;
+};
+
+const toggleModal = () => {
+  openModal.value = !openModal.value;
 };
 // get user
 onMounted(async () => {
