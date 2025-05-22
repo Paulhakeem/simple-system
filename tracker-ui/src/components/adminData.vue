452d62a9416b98b65861777183594bb0c5dd9ed7
Paul <paulnyamawi18@gmail.com>
2025-05-22 22:01:24 +0300

    code

--- tracker-ui/src/components/adminData.vue
+++ tracker-ui/src/components/adminData.vue
@@ -22,7 +22,7 @@
         </form>
         <!-- END OF FORM -->
         <div class="flex">
-          <p class="text-lg text-gray-500 mt-2">Percentage(%)</p>
+          <p class="text-xl text-gray-500 mt-2">Percentage(%)</p>
         </div>
       </div>
       <div
@@ -57,70 +57,8 @@
           </div>
         </div>
       </div>
-      <div
-        class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4"
-      >
-        <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
-          <div class="rounded-full bg-grey inline-flex mr-3">
-            <svg
-              class="fill-current text-white h-8 w-8 block"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 38 38"
-            >
-              <g fill-rule="evenodd">
-                <path
-                  d="M12.29 28.04l1.29-5.52-1.58.67.63-2.85 1.64-.68L16.52 10h5.23l-1.52 7.14 2.09-.74-.58 2.7-2.05.8-.9 4.34h8.1l-.99 3.8z"
-                ></path>
-              </g>
-            </svg>
-          </div>
-          <span class="text-lg">Litecoin</span>
-        </div>
-        <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
-          <div class="bg-grey h-2 w-2 rounded-full mr-2"></div>
-          0%
-        </div>
-        <div class="flex w-3/5 md:w/12">
-          <div class="w-1/2 px-4">
-            <div class="text-right">0.0000 LTC</div>
-          </div>
-          <div class="w-1/2 px-4">
-            <div class="text-right text-grey">CA$0.00</div>
-          </div>
-        </div>
-      </div>
-      <div
-        class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4"
-      >
-        <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
-          <div class="rounded-full bg-indigo inline-flex mr-3">
-            <svg
-              class="fill-current text-white h-8 w-8 block"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-            >
-              <g fill-rule="evenodd">
-                <path
-                  d="M10.13 17.76c-.1-.15-.06-.2.09-.12l5.49 3.09c.15.08.4.08.56 0l5.58-3.08c.16-.08.2-.03.1.11L16.2 25.9c-.1.15-.28.15-.38 0l-5.7-8.13zm.04-2.03a.3.3 0 0 1-.13-.42l5.74-9.2c.1-.15.25-.15.34 0l5.77 9.19c.1.14.05.33-.12.41l-5.5 2.78a.73.73 0 0 1-.6 0l-5.5-2.76z"
-                ></path>
-              </g>
-            </svg>
-          </div>
-          <span class="text-lg">Ethereum</span>
-        </div>
-        <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
-          <div class="bg-indigo h-2 w-2 rounded-full mr-2"></div>
-          0%
-        </div>
-        <div class="flex w-3/5 md:w/12">
-          <div class="w-1/2 px-4">
-            <div class="text-right">0.0000 ETH</div>
-          </div>
-          <div class="w-1/2 px-4">
-            <div class="text-right text-grey">CA$0.00</div>
-          </div>
-        </div>
-      </div>
+      
+      <!--  -->
       <div class="px-6 py-4">
         <div class="text-center text-grey">Total Balance &asymp; CA$21.28</div>
       </div>
