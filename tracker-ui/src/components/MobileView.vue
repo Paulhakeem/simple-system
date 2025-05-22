b96b7bfa30a592acab7c32505b51e1afc8333dba
Paul <paulnyamawi18@gmail.com>
2025-05-22 07:24:54 +0300

    goodmorning

---
+++ tracker-ui/src/components/MobileView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="px-6 lg:hidden mb-10">
+    <div class="text-center bg-[#1796d5] p-8 mt-8">
+      <div class="text-sm uppercase text-gray-200 font-semibold tracking-wide">
+        Total Income
+      </div>
+      <span class="font-bold text-2xl text-gray-200"> Ksh100,000 </span>
+    </div>
+    <div class="bg-[#1796d5] p-8 mt-8 text-center">
+      <div class="text-sm uppercase text-gray-200 font-semibold tracking-wide">
+        Total Expenses
+      </div>
+      <span class="font-bold text-2xl  text-red-300"> Ksh60,000 </span>
+    </div>
+    <div class="text-center bg-[#1796d5] p-8 mt-8">
+      <div>
+        <div
+          class="text-sm uppercase text-gray-200 font-semibold tracking-wide"
+        >
+          Last month (%)
+        </div>
+        <span class="font-bold text-2xl text-gray-200"> Ksh40,000 </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- tracker-ui/src/views/userDashboard.vue
+++ tracker-ui/src/views/userDashboard.vue
@@ -170,40 +170,8 @@
         <div
           class="bg-gray-300 border-t border-b sm:border-l sm:border-r sm:rounded shadow mb-6"
         >
-          <div class="flex items-center px-6 lg:hidden">
-            <div class="flex-grow flex-no-shrink py-6">
-              <div class="text-grey-darker mb-2">
-                <span class="text-3xl align-top">CA$</span>
-                <span class="text-5xl">21,404</span>
-                <span class="text-3xl align-top">.74</span>
-              </div>
-              <div class="text-green-light text-sm">
-                &uarr; CA$12,955.35 (154.16%)
-              </div>
-            </div>
-            <div class="flex-shrink w-32 inline-block relative">
-              <select
-                class="block appearance-none w-full bg-white border border-grey-light px-4 py-2 pr-8 rounded"
-              >
-                <option>BTC</option>
-                <option>ETH</option>
-                <option>LTC</option>
-              </select>
-              <div
-                class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey"
-              >
-                <svg
-                  class="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
+        <MobileView/>
+          <!--  -->
           <div class="hidden lg:flex">
             <div class="w-1/3 text-center py-8">
               <div class="border-r">
@@ -216,21 +184,23 @@
             <div class="w-1/3 text-center py-8">
               <div class="border-r">
                 <div class="text-sm uppercase text-gray-600 font-semibold  tracking-wide">
-                 This month (Income)
+                 Total Expenses
                 </div>
-                <span class="font-bold text-2xl text-green-500"> Ksh60,000 </span>
+                <span class="font-bold text-2xl text-red-500"> Ksh60,000 </span>
               </div>
             </div>
             <div class="w-1/3 text-center py-8">
               <div>
                 <div class="text-sm uppercase text-gray-600 font-semibold tracking-wide">
-                  Last month (%)
+                   (%)
                 </div>
                 <span class="font-bold text-2xl text-green-500"> Ksh40,000 </span>
               </div>
             </div>
           </div>
+          <!--  -->
         </div>
+        <!--  -->
         <div class="flex flex-wrap -mx-4">
           <div class="w-full mb-6 lg:mb-0 lg:w-1/2 px-4 flex flex-col">
             <div
@@ -438,6 +408,7 @@
 import { useUserStore } from "../../store/user";
 import { useShopStore } from "../../store/shops";
 import { useDeleteStore } from "../../store/delete";
+import MobileView from "../components/MobileView.vue";
 import { ref } from "vue";
 
 const { user } = useUserStore();
