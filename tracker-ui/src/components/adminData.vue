47db982e32126cccf57a4b84c9c905df94e69a8b
Paul <paulnyamawi18@gmail.com>
2025-05-23 21:47:33 +0300

    adding content

--- tracker-ui/src/components/adminData.vue
+++ tracker-ui/src/components/adminData.vue
@@ -3,7 +3,7 @@
     <div
       class="flex-grow flex flex-col bg-gray-200 border-t border-b sm:rounded sm:border shadow overflow-hidden"
     >
-      <div class="flex justify-between px-6 mb-px">
+      <div class="flex justify-between px-6 mb-px pt-3">
         <form class="flex gap-2">
           <input
             type="text"
@@ -25,43 +25,23 @@
           <p class="text-xl text-gray-500 mt-2">Percentage(%)</p>
         </div>
       </div>
-      <div
-        class="flex-grow flex px-6 py-6 text-grey-darker items-center border-b -mx-4"
-      >
-        <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
-          <div class="rounded-full bg-orange-400 inline-flex mr-3">
-            <svg
-              class="fill-current text-white h-8 w-8 block"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-            >
-              <g fill-rule="evenodd">
-                <path
-                  d="M21.78 15.37c.51-.61.83-1.4.83-2.26 0-2.74-1.6-4.38-4.24-4.38V5.45c0-.12-.1-.22-.22-.22h-1.27c-.11 0-.2.1-.2.21v3.3h-1.7V5.44c0-.12-.1-.22-.22-.22H13.5c-.12 0-.2.1-.21.21v3.3H9.67c-.12 0-.21.09-.21.21v1.31c0 .12.1.22.21.22h.21c.94 0 1.7.79 1.7 1.75v7c0 .92-.68 1.67-1.55 1.75a.21.21 0 0 0-.18.16l-.33 1.32c-.01.06 0 .13.04.19.04.05.1.08.17.08h3.55v3.3c0 .1.1.2.2.2h1.28c.12 0 .21-.1.21-.22v-3.28h1.7v3.3c0 .1.1.2.21.2h1.27c.12 0 .22-.1.22-.22v-3.28h.85c2.65 0 4.24-1.64 4.24-4.37 0-1.28-.68-2.39-1.68-3zm-6.8-4.01h2.54c.94 0 1.7.78 1.7 1.75 0 .96-.76 1.75-1.7 1.75h-2.55v-3.5zm3.39 8.75h-3.4v-3.5h3.4c.93 0 1.7.78 1.7 1.75 0 .96-.77 1.75-1.7 1.75z"
-                ></path>
-              </g>
-            </svg>
+      <div v-for="shop in shops" :key="shop.id">
+        <p class="text-gray-400">{{ shop.name }}</p>
+        <!-- <div v-if="user._id === shop.userId"
+          class="flex-grow flex px-6 py-2 text-grey-darker items-center border-b mx-4"
+        >
+          <div class="w-2/5 xl:w-1/4 px-4 flex items-center">
+            <div class="rounded-full bg-orange-400 inline-flex mr-3">
+              <font-awesome-icon
+                icon="fa-solid fa-circle"
+                class="text-orange-400 text-lg"
+              />
+            </div>
+            <span class="text-lg">{{ shop.name }}</span>
           </div>
-          <span class="text-lg">Bitcoin</span>
-        </div>
-        <div class="hidden md:flex lg:hidden xl:flex w-1/4 px-4 items-center">
-          <div class="bg-orange h-2 rounded-full flex-grow mr-2"></div>
-          100%
-        </div>
-        <div class="flex w-3/5 md:w/12">
-          <div class="w-1/2 px-4">
-            <div class="text-right">0.0010 BTC</div>
-          </div>
-          <div class="w-1/2 px-4">
-            <div class="text-right text-grey">CA$21.28</div>
-          </div>
-        </div>
+        </div> -->
       </div>
-      
       <!--  -->
-      <div class="px-6 py-4">
-        <div class="text-center text-grey">Total Balance &asymp; CA$21.28</div>
-      </div>
     </div>
   </div>
 </template>
--- tracker-ui/src/views/userDashboard.vue
+++ tracker-ui/src/views/userDashboard.vue
@@ -1,89 +1,4 @@
-<!-- <template>
-  <transition name="modal-outer">
-    <div
-      class="absolute w-full z-50 h-screen top-0 left-0 flex justify-center px-8"
-    >
-      <transition name="modal-inner">
-        <div class="p-4 self-start">
-       \
-          <div class="grid min-h-screen place-content-center">
-            <div
-              class="bg-white p-4 shadow-lg rounded-xl max-w-xl mx-auto relative"
-            >
-              <header
-                class="bg-[#1796d5] transform flex items-center mb-10 py-8 px-6"
-              >
-              
 
-                <div class="ml-5">
-                  <h1
-                    class="text-white tracking-wide text-lg first-letter:uppercase"
-                  >
-                    {{ user.name }}
-                  </h1>
-                  <p class="text-gray-300 tracking-wider text-sm">Admin</p>
-                </div>
-              </header>
-
-              <div>
-                <button
-                  @click="toggleInput"
-                  class="text-white capitalize text-sm bg-[#1796d5] inline-block rounded-md items-center py-2 pl-3 pr-4 shadow-md mx-auto tracking-wider mb-5 cursor-pointer"
-                >
-                  <font-awesome-icon :icon="['fas', 'plus']" />
-                  {{ Input ? "close" : "add shop branch" }}
-                </button>
-         
-                <div
-                  v-if="Input"
-                  class="fixed max-w-md text-right bg-slate-400 p-4 rounded-tr-lg rounded-br-lg rounded-bl-2xl z-50"
-                >
-                  <div class="relative inline-block justify-center">
-                    <input
-                      v-model="shopName"
-                      placeholder="Shop Name"
-                      @keyup.enter="createShop"
-                      type="text"
-                      class="border-2 border-gray-300 rounded-md px-2 py-1 w-36 text-white bg-gray-800 focus:outline-none focus:border-[#1796d5] focus:ring-1 focus:ring-[#1796d5] placeholder:text-gray-400"
-                    />
-                    <button
-                      @click="createShop"
-                      class="bg-[#1796d5] text-white rounded-md px-2 py-1 ml-2 cursor-pointer hover:bg-[#1796d5]/80 focus:outline-none focus:ring-2 focus:ring-[#1796d5] focus:ring-offset-2"
-                    >
-                      Add
-                    </button>
-                  </div>
-                </div>
-              
-                <p class="text-xs text-gray-500 font-medium">Manage Shops</p>
-              </div>
-              <ul v-for="shop in shops" :key="shop._id" class="px-8 relative">
-                <li
-                  v-if="user._id === shop.userId"
-                  class="flex items-center text-gray-900 text-md py-2"
-                >
-                  <div class="cursor-pointer">
-                    <span class="text-[#1796d5] mr-5"
-                      ><font-awesome-icon :icon="['fas', 'circle-check']"
-                    /></span>
-                    {{ shop.name }}
-                  </div>
-                  <span @click="deleteShop(shop._id)"
-                    ><font-awesome-icon
-                      :icon="['fas', 'trash']"
-                      class="text-red-500 ml-8 text-sm cursor-pointer"
-                  /></span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </transition>
-    </div>
-  </transition>
-</template>
-
- -->
 
 <template>
   <div>
@@ -195,7 +110,7 @@
                 <div class="text-sm uppercase text-gray-600 font-semibold tracking-wide">
                    (%)
                 </div>
-                <span class="font-bold text-2xl text-green-500"> Ksh40,000 </span>
+                <span class="font-bold text-2xl text-green-500"> 70% </span>
               </div>
             </div>
           </div>
@@ -211,8 +126,8 @@
             <div class="bg-white border-t border-b sm:rounded sm:border shadow">
               <div class="border-b">
                 <div class="flex justify-between px-6 -mb-px">
-                  <h3 class="text-blue-dark py-4 font-normal text-lg">
-                    Recent Activity
+                  <h3 class="text-gray-600 py-4 font-normal text-lg">
+                    Imported transactions
                   </h3>
                 </div>
               </div>
@@ -220,31 +135,18 @@
                 <div class="text-center px-6 py-4">
                   <div class="py-8">
                     <div class="mb-4">
-                      <svg
-                        class="inline-block fill-current text-grey h-16 w-16"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          d="M11.933 13.069s7.059-5.094 6.276-10.924a.465.465 0 0 0-.112-.268.436.436 0 0 0-.263-.115C12.137.961 7.16 8.184 7.16 8.184c-4.318-.517-4.004.344-5.974 5.076-.377.902.234 1.213.904.959l2.148-.811 2.59 2.648-.793 2.199c-.248.686.055 1.311.938.926 4.624-2.016 5.466-1.694 4.96-6.112zm1.009-5.916a1.594 1.594 0 0 1 0-2.217 1.509 1.509 0 0 1 2.166 0 1.594 1.594 0 0 1 0 2.217 1.509 1.509 0 0 1-2.166 0z"
-                        />
-                      </svg>
+                      <font-awesome-icon :icon="['fas', 'rocket']" class="text-6xl text-[#1796d5]"/>
                     </div>
-                    <p class="text-2xl text-grey-darker font-medium mb-4">
-                      No buys or sells yet
+                    <p class="text-2xl text-gray-600 font-medium mb-4">
+                      No imported transaction!
                     </p>
                     <p class="text-grey max-w-xs mx-auto mb-6">
-                      You've successfully linked a payment method and can start
-                      buying digital currency.
+                     Import all of your transactions before 24hrs to avaoid ovaloading of data.
+                      <br />
+                      <span class="text-red-500 font-semibold">
+                        Note: You can only import 1000 transactions at a time from each shop.
+                      </span>
                     </p>
-                    <div>
-                      <button
-                        type="button"
-                        class="bg-blue hover:bg-blue-dark text-white border border-blue-dark rounded px-6 py-4"
-                      >
-                        Buy now
-                      </button>
-                    </div>
                   </div>
                 </div>
               </div>
